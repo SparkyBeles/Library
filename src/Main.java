be352de2080da01b0be674fa7f2f6d3e54382d98
beles <beles.dev@gmail.com>
2024-09-30 15:11:40 +0200

    Fixed the issue where the loaned books list keeps growing when returning a book!

--- src/Main.java
+++ src/Main.java
@@ -36,10 +36,10 @@ public class Main {
                                     loanBook(sc, bookLoanedList, bookList);
                                     break;
                                 }
-                                case 2:
+                                case 2: {
                                     returnBook(sc, bookLoanedList, bookList);
                                     break;
-
+                                }
 
                                 default:
                                     System.out.println("Invalid choice");
@@ -60,87 +60,86 @@ public class Main {
 
     }
 
+    //Method for borrowing books!
     private static void loanBook(Scanner sc, ArrayList<Book> bookList, ArrayList<Book> bookLoanedList) {
         if (bookList.isEmpty()){
-            System.out.println("Alla böcker är utlånade!");
+            System.out.println("Du har inga lånade böcker");
             return;
         }
 
-        DisplayAvailableBooks(sc, bookList, bookLoanedList);
+        DisplayAvailableBooks(bookList);
 
 
         try {
+
             int choice = sc.nextInt();
             int bookIndex = choice -1;
             if (bookIndex >= 0 && bookIndex < bookList.size()){
                 Book chosenBook = bookList.get(bookIndex);
                 bookLoanedList.add(chosenBook);
                 bookList.remove(chosenBook);
-                System.out.println("Du har lånad boken " + chosenBook.title);
+                System.out.println("Du har lämnat boken " + chosenBook.title);
+            } else if (choice >0 && choice >= bookList.size()){
+                System.out.println("fel försök igen!");
+                sc.nextLine();
             }
-            else if (choice == 0) {
-                return;
-
-
-
-            }
-
 
         }catch (InputMismatchException e){
+
+
             System.out.println("Ogiltigt val 1:låna 2:lämna  0:Avsluta ");
             sc.nextLine();
-        }
+         }
 
-       }
+    }
 
 
+       //Method for returning books
        private static void returnBook(Scanner sc, ArrayList<Book> bookList, ArrayList<Book> bookLoanedList) {
          if (bookLoanedList.isEmpty()){
-             System.out.println("Inga böcker lånade!");
+             System.out.println("Alla böcker är utlånade!");
              return;
          }
 
-         DisplayLoanedBooks(sc, bookList, bookLoanedList);
+         DisplayLoanedBooks(bookLoanedList);
 
-
-
-            try {
+         try {
 
                 int choice = sc.nextInt();
                 int bookIndex = choice - 1;
                 if (choice == 0){
-                    return;
                 }
 
                 else if (bookIndex >= 0 && bookIndex < bookLoanedList.size()){
-                    Book chocedBook =bookLoanedList.get(bookIndex);
-                    bookLoanedList.add(chocedBook);
-                    bookList.remove(chocedBook);
-                    System.out.println("Du har lånat boken: " + chocedBook.title);
+                    Book chosenBook =bookLoanedList.get(bookIndex);
+                    bookList.add(chosenBook);
+                    bookLoanedList.remove(chosenBook);
+                    System.out.println("Du har lånat boken: " + chosenBook.title);
+                } else if (choice >0 && choice >= bookLoanedList.size()) {
+                    System.out.println("Ogiltig val försök igen!");
+
                 }
 
-            }catch (InputMismatchException e){
-                System.out.println("Fel input försök igen eller 0");
+         }catch (InputMismatchException e){
+                System.out.println("Fel input försök igen eller 0 för avsluta ");
                 sc.nextLine();
-            }
+             }
 
-           }
+    }
 
 
-           private static void DisplayLoanedBooks (Scanner sc, ArrayList<Book> bookList, ArrayList<Book> bookLoanedList) {
+           //Method for displaying books loaned!
+           private static void DisplayLoanedBooks (ArrayList<Book> bookLoanedList) {
                for (int i = 0; i < bookLoanedList.size() ; i++) {
-                   System.out.println((i+1) +"." + "Tillgängliga böcker är! " + bookLoanedList.get(i).title);
+                   System.out.println((i+1) +"." + "Lånad bok!:" + bookLoanedList.get(i).title);
                    
                }
+    }
 
-               
-        
-
-           }
-           
-           private static void DisplayAvailableBooks(Scanner sc, ArrayList<Book> bookList, ArrayList<Book> bookLoanedList) {
+           //Method for displaying books available to loan
+           private static void DisplayAvailableBooks(ArrayList<Book> bookList) {
                for (int i = 0; i < bookList.size() ; i++) {
-                   System.out.println((i+1) +"."+ "Lånade böcker är! " + bookList.get(i).title);
+                   System.out.println((i+1) +"."+ "Tillgänglig bok!:" + bookList.get(i).title);
 
                }
 
